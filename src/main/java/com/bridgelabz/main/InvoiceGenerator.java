3a062464e973ad51e8b16187d5f4d8071f96845c
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-01 11:29:55 +0530

    Added: Enhanced Invoice program

--- src/main/java/com/bridgelabz/main/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/main/InvoiceGenerator.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.main;
 
-public class InvoiceGenerator {
 
+public class InvoiceGenerator {
+    //constant variables
     private static final double MINIMUM_COST_PER_KM = 10.0;
     private static final int COST_PER_TIME = 1;
     private static final int MINIMUM_FARE = 5;
@@ -9,17 +10,24 @@ public class InvoiceGenerator {
     //calculate the total fare
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-        if (totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        else
-            return totalFare;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
+
     //calculating Fare for Multiple rides
-    public double calculateFare(Ride[] rides){
+    public double calculateFare(Ride[] rides) {
         double totalFare = 0;
-        for (Ride ride : rides){
+        for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return totalFare;
+    }
+
+    //calculating totalFare, average Fare for multiple rides
+    public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
     }
 }
--- src/test/java/com/bridgelabz/test/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/test/InvoiceServiceTest.java
@@ -1,37 +1,60 @@
 package com.bridgelabz.test;
 
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.bridgelabz.main.InvoiceGenerator;
+import com.bridgelabz.main.InvoiceSummary;
 import com.bridgelabz.main.Ride;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
+    // testCase to write Total fare
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
 
+    //testCase to calculate the Minimum Charges
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
 
     }
+
+    //testCase for return total Fare for multiple Rides
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
         double fare = invoiceGenerator.calculateFare(rides);
         Assert.assertEquals(30, fare, 0.0);
     }
+
+    //test case for return the Invoice Summary
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(5.0, 10),
+                new Ride(10, 20)
+        };
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(rides);
+        InvoiceSummary summary = new InvoiceSummary(2, 180);
+        Assert.assertEquals(summary, invoiceSummary);
+    }
+
 }
